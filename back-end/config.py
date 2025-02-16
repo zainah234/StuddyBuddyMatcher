b1199077c0324405729ee3a1139f2a3d09b77ed1
AdelinaPatlatii <adelina.patlatii@mail.utoronto.ca>
2025-02-16 10:49:02 -0500

    Tiny modification

--- back-end/config.py
+++ back-end/config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = f"postgresql://postgres:zainahibah:password@localhost:5432/study_buddies"
+    SQLALCHEMY_DATABASE_URI = f"postgresql://[user]:[password]@localhost:5432/study_buddies"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
